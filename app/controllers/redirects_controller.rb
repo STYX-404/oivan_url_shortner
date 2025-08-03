cdd0470307b6276575f23c062ab1704a0864e300
ahmed_taha <ahmed.99.tahaa@gmail.com>
2025-08-03 20:07:27 +0300

    feat: add RedirectsController for handling URL redirects

---
+++ app/controllers/redirects_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Controller for handling URL redirects
+class RedirectsController < ApplicationController
+  before_action :set_url, only: %i[show]
+  def show
+    if @url
+      redirect_to @url.original_url, allow_other_host: true
+    else
+      head :not_found
+    end
+  end
+
+  private
+
+  def set_url
+    uniq_key = params[:uniq_key]
+    @url = Url.find_by(short_url: "#{ENV.fetch('DOMAIN_URL')}/#{uniq_key}")
+  end
+end
