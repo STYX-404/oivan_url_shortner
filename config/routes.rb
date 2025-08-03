835990f26c4ed9aa4268661bd78823a044622f56
ahmed_taha <ahmed.99.tahaa@gmail.com>
2025-08-03 20:09:10 +0300

    feat: add API routes for URL encoding and decoding, and mount Rswag for API documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/:uniq_key', to: 'redirects#show'
+
+  namespace :api do
+    namespace :v1 do
+      resources :urls, only: %i[] do
+        collection do
+          post :encode
+          get :decode
+        end
+      end
+    end
+  end
 end
